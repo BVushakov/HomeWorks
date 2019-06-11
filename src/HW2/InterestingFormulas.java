3dc0fe892be0eb0370984d4210b4dfd32eb45c6b
dn281091ubv <bogdan.ushakov@privatbank.ua>
2019-06-11 23:37:10 +0300

    HW_2 Second half. Begin(#9)

---
+++ src/HW2/InterestingFormulas.java
@@ -0,0 +1,16 @@
+package HW2;
+
+import static java.lang.Math.*;
+
+public class InterestingFormulas {
+    public static void main(String...args) {
+
+        /** Formula 9*/
+        double x = 25.15;
+        double result9 = Math.pow(1.1, x) + Math.abs(Math.cos(Math.sqrt(PI * x))) - (3.0 / 8.0);
+        System.out.println(result9);
+        System.out.println(3.0 / 8.0);
+
+
+    }
+}
