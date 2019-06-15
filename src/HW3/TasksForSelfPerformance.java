5b5c1c7798138ae47eaa775df731cc909558eb59
dn281091ubv <bogdan.ushakov@privatbank.ua>
2019-06-15 15:12:01 +0300

    HW_3 Tasks for self Performance(almost all)

--- src/HW3/TasksForSelfPerformance.java
+++ src/HW3/TasksForSelfPerformance.java
@@ -2,6 +2,9 @@ package HW3;
 
 public class TasksForSelfPerformance {
 
+    public static final int MINNUMBER = -5;
+    public static final int MAXNUMBER = 3;
+
     public static void main(String[] args) {
 
         /** Задача со скидкой */
@@ -19,7 +22,7 @@ public class TasksForSelfPerformance {
 
 
         /** Задача с площадью круга и квадрата
-         *
+
         Получить радиус круга: d = (√S : π) * 2 //d - диаметр, S - площадь
         Получить сторону квадрата: S = s2 //s - сторона, S - площадь
         Получить сторону квадрата: d = s√2 //d - диагональ  */
@@ -55,20 +58,63 @@ public class TasksForSelfPerformance {
         System.out.println("");
 
 
+        /** Задача с нахождением делителя */
+
+        int aNum = 3;
+        int bNum = 6;
+
+        if (bNum % aNum == 0) {
+            System.out.println("Число " + aNum + " является делителем числа " + bNum);
+        } else {
+            System.out.println("Число " + aNum + " не является делителем числа " + bNum);
+        }
+        System.out.println("");
 
 
+        /** Задача. Принадлежит ли число к интервалу */
 
 
+        int myNumber = 0;
+
+        if (myNumber >= MINNUMBER && myNumber <= MAXNUMBER) {
+            System.out.println("Число " + myNumber + " входит в диапазон от " + MINNUMBER + " до " + MAXNUMBER + ".");
+        } else {
+            System.out.println("Число " + myNumber + " не входит в диапазон от " + MINNUMBER + " до " + MAXNUMBER + ".");
+        }
+        System.out.println("");
 
 
+        /** Задача про треугольник */
+
+        int sideA = 3;
+        int sideB = 3;
+        int sideC = 3;
+
+        if (sideA == sideB && sideB == sideC) {
+            System.out.println("Треугольник является равнобедренным.");
+        } else {
+            System.out.println("Треугольник не является равнобедренным.");
+        }
+        System.out.println("");
 
 
+        /** Задача с разными числами */
 
+        //Самое большое
 
-
-
-
-
-
+        int firstNumber = 2;
+        int secondNumber = 8;
+        int thirdNumber = 5;
+//
+        if (firstNumber > secondNumber) {
+            if (secondNumber > thirdNumber){
+                System.out.println("Cреднее число " + secondNumber);
+            } else {
+                System.out.println("Cреднее число " + thirdNumber);
+            }
+            System.out.println("Cреднее число " + firstNumber);
+        } else {
+            System.out.println("Cреднее число " + thirdNumber);
+        }
     }
 }
