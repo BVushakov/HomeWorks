545d394af6113263f6735700b6b9313d37783cae
dn281091ubv <bogdan.ushakov@privatbank.ua>
2019-06-22 23:34:24 +0300

    HW_4 Task#5 - done

---
+++ src/HW4/ArraySorting.java
@@ -0,0 +1,20 @@
+package HW4;
+
+import java.util.Arrays;
+
+public class ArraySorting {
+    public static void main(String[] args) {
+
+        int[] arr = {0, 123, -12, 991, 89, 66, 21, 89};
+        int temp;
+
+        for (int i = 0; i < arr.length; i++)
+            for (int j = i + 1; j < arr.length; j++)
+                if (arr[j] < arr[i]) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+        System.out.println(Arrays.toString(arr));
+    }
+}
