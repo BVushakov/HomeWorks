1e7f910124c25a53f97542312bd6dd4efef73b99
dn281091ubv <bogdan.ushakov@privatbank.ua>
2019-06-22 22:51:00 +0300

    HW_4 Task#3 - done

---
+++ src/HW4/FindTheSmallestNumber.java
@@ -0,0 +1,16 @@
+package HW4;
+
+public class FindTheSmallestNumber {
+    public static void main(String[] args) {
+
+        int[] simpleArray = {1, 21, -3, 0, 18};
+        int min = simpleArray[0];
+
+        for(int i = 0; i != simpleArray.length; i ++){
+            if(simpleArray[i] < min){
+                min = simpleArray[i];
+            }
+        }
+        System.out.println("Минимальное число в массиве: " + min);
+    }
+}
