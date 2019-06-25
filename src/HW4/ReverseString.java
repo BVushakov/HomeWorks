fdf583d543876256abce44db4d806ce4a9cb707b
dn281091ubv <bogdan.ushakov@privatbank.ua>
2019-06-25 15:08:48 +0300

    HW_4 Task7# - done

---
+++ src/HW4/ReverseString.java
@@ -0,0 +1,14 @@
+package HW4;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String str = "Hello Alexey";
+        System.out.println("Оригинальная строка - " + str);
+
+        char[] array = str.toCharArray();
+        System.out.print("Перевернутая строка - ");
+        for (int i = array.length - 1; i >= 0; i--) {
+            System.out.print(array[i]);
+        }
+    }
+}
