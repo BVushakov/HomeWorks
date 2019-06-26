88791864780fc67b08b45df18f108072af9b9d6e
dn281091ubv <bogdan.ushakov@privatbank.ua>
2019-06-26 14:02:09 +0300

    HW_4 Task9# - done

---
+++ src/HW4/FibonacciNumber.java
@@ -0,0 +1,21 @@
+package HW4;
+
+public class FibonacciNumber {
+    public static void main(String[] args) {
+        int f = 0;
+        int f1 = 1;
+        int f2 = 0;
+        int stopNumber = 500;
+
+        System.out.print( "0 ");
+        for(int i = 3; i <= stopNumber; i++){
+            f = f1;
+            f1 = f2;
+            f2 = f + f1;
+            if (f2 > stopNumber) {
+            break;
+            }
+            System.out.print(f2 + " ");
+        }
+    }
+}
