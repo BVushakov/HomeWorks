798c6d0cb7c8c30327a0d59b92dff3bb8b6a29d7
dn281091ubv <bogdan.ushakov@privatbank.ua>
2019-06-26 17:29:55 +0300

    HW_4 Task6# - done

---
+++ src/HW4/PalindromePrint.java
@@ -0,0 +1,18 @@
+package HW4;
+
+public class PalindromePrint {
+    public static void main(String[] args) {
+
+        String s = "Голод долог".toUpperCase();
+        boolean b = true;
+
+        for (int i = 0; i < s.length() / 2 + 1; i++) {
+            if (s.toCharArray()[i] != s.toCharArray()[s.length() - i - 1]) {
+                b = !b;
+                break;
+            }
+        }
+        System.out.println(b ? "Палиндром " + s : "Слово НЕ палиндром " + s);
+    }
+
+}
