840ae456773303ee4fe0c04baa29e19294ff6b59
dn281091ubv <bogdan.ushakov@privatbank.ua>
2019-06-26 18:24:24 +0300

    HW_4 Task2# - done

---
+++ src/HW4/ConvertingHexadecimalToDecimal.java
@@ -0,0 +1,21 @@
+package HW4;
+
+import java.util.Scanner;
+
+public class ConvertingHexadecimalToDecimal {
+    public static void main(String[] args) {
+        System.out.println("Hexadecimal to Decimal");
+        Scanner input = new Scanner(System.in);
+
+        while ( true ) {
+            System.out.print("Hex: ");
+            if ( ! input.hasNextInt(16) )
+                break;
+
+            int n = input.nextInt(16);
+            System.out.printf("Dec: %d", n);
+            System.out.println("");
+        }
+        input.close();
+    }
+}
